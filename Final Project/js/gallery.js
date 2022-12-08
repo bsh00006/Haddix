df8c9345143cb00e244d1853f8a3b2d83468c5f3
bsh00006 <112179328+bsh00006@users.noreply.github.com>
2022-12-08 14:38:55 -0500

    Add files via upload

---
+++ Final Project/js/gallery.js
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<meta name=”author” content=”Brian Haddix”>
+		<title>Photo Gallery</title>
+	
+
+		<style>
+		</style>
+	</head>
+<body>
+		<h1>Creating a Photo Gallery with CSS & JavaScript</h1>
+		<footer role="contentinfo">
+		This gallery was created using the YouTube tutorial: <a href="https://youtu.be/sHG3Sf6XN9g">How To Make Image Gallery Using HTML, CSS & JavaScript</a>
+		</footer>
+</body>
+</html>
